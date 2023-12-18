202a679b30a3dd3c4abaacac88b9a851245902ee
Dave <david.forbes.jr@gmail.com>
2023-12-18 09:35:04 -0500

    Update EnvironmentVariables.cpp
    Cleaning up tabs.

--- survey/EnvironmentVariables.cpp
+++ survey/EnvironmentVariables.cpp
@@ -3,27 +3,27 @@
 
 int main()
 {
-	// Grab the systems environment variables
+    // Grab the systems environment variables
     LPWSTR envStrings = GetEnvironmentStrings();
 
     // Make sure we're not dereferecing a NULL pointer
-	if (envStrings == NULL)
-	{
-		fprintf(stderr, "Error: envStrings are NULL: %ld\n", GetLastError());
-		return EXIT_FAILURE;
-	}
+    if (envStrings == NULL)
+    {
+	fprintf(stderr, "Error: envStrings are NULL: %ld\n", GetLastError());
+	return EXIT_FAILURE;
+    }
 
-	// Setup our Iterator
-	LPWSTR curEnvStr = envStrings;
-	fprintf(stdout, "[+] Grabbing System Environment Variables [+]\n");
+    // Setup our Iterator
+    LPWSTR curEnvStr = envStrings;
+    fprintf(stdout, "[+] Grabbing System Environment Variables [+]\n");
 	
     // Iterate over the environment variables and print to stdout
-	while (*curEnvStr != L'\0')
-	{
-		fprintf(stdout, "\t[+] %ws\n", curEnvStr);
-		curEnvStr += lstrlenW(curEnvStr) + 1;
-	}
+    while (*curEnvStr != L'\0')
+    {
+	fprintf(stdout, "\t[+] %ws\n", curEnvStr);
+	curEnvStr += lstrlenW(curEnvStr) + 1;
+    }
 
-	// Cleanup
-	FreeEnvironmentStrings(envStrings);
+    // Cleanup
+    FreeEnvironmentStrings(envStrings);
 }
