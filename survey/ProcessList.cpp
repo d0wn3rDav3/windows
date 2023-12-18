4d46f73149354a85bed4c06c1b05f2c0fffb340a
Dave <david.forbes.jr@gmail.com>
2023-12-18 11:10:54 -0500

    Update ProcessList.cpp
    Cleaning up tab nonsense.

--- survey/ProcessList.cpp
+++ survey/ProcessList.cpp
@@ -6,62 +6,62 @@ constexpr auto PID_LIST_SIZE = 1024;		// Global for the size of the list
 
 INT main()
 {
-	// Setup Process Variables
-	DWORD dwProcList[PID_LIST_SIZE] = { 0 };
-	DWORD dwRealSize = 0;
-	BOOL bResult = FALSE; // Return type of BOOL
-	DWORD32 dwCount = 0;
+// Setup Process Variables
+    DWORD dwProcList[PID_LIST_SIZE] = { 0 };
+    DWORD dwRealSize = 0;
+    BOOL bResult = FALSE; // Return type of BOOL
+    DWORD32 dwCount = 0;
 
-	// Enumerate the running processes
-	bResult = EnumProcesses(dwProcList, sizeof(dwProcList), &dwRealSize);
+    // Enumerate the running processes
+    bResult = EnumProcesses(dwProcList, sizeof(dwProcList), &dwRealSize);
 
-	// Make sure bResult is not FALSE
-	if (!bResult)
+    // Make sure bResult is not FALSE
+    if (!bResult)
+    {
+	fprintf(stderr, "[!] Error: EnumProcesses: %ld", GetLastError());
+	return FALSE;
+    }
+
+    // Get the number of running processes
+    dwCount = dwRealSize / sizeof(DWORD);
+
+    // Iterate over our process list
+    for (DWORD dwIndex = 0; dwIndex < dwCount; dwIndex++)
+    {
+	// Open a HANDLE to the current process
+	HANDLE hProcess = OpenProcess(
+		PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_VM_READ,
+		FALSE,
+		dwProcList[dwIndex]
+	);
+
+	// Make sure the handle is there.
+	if (hProcess)
 	{
-		fprintf(stderr, "[!] Error: EnumProcesses: %ld", GetLastError());
-		return FALSE;
+	    wchar_t szProcessPath[MAX_PATH];
+	    DWORD dwProcNameLen = GetModuleFileNameExW(
+		hProcess,
+		NULL,
+		szProcessPath,
+		MAX_PATH
+	    );
+	    if (dwProcNameLen > 0)
+	    {
+	        wprintf(L"[+] PID: %d - %ws\n", dwProcList[dwIndex], szProcessPath);
+	    }
+	    else
+	    {
+	        fprintf(stderr, "[!] Error: GetModuleBaseNameW: %ld\n", GetLastError());
+		return EXIT_FAILURE;
+            }
+	    CloseHandle(hProcess);
 	}
-
-	// Get the number of running processes
-	dwCount = dwRealSize / sizeof(DWORD);
-
-	// Iterate over our process list
-	for (DWORD dwIndex = 0; dwIndex < dwCount; dwIndex++)
+	else
 	{
-		// Open a HANDLE to the current process
-		HANDLE hProcess = OpenProcess(
-			PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_VM_READ,
-			FALSE,
-			dwProcList[dwIndex]
-		);
-
-		// Make sure the handle is there.
-		if (hProcess)
-		{
-			wchar_t szProcessPath[MAX_PATH];
-			DWORD dwProcNameLen = GetModuleFileNameExW(
-				hProcess,
-				NULL,
-				szProcessPath,
-				MAX_PATH
-			);
-			if (dwProcNameLen > 0)
-			{
-				wprintf(L"[+] PID: %d - %ws\n", dwProcList[dwIndex], szProcessPath);
-			}
-			else
-			{
-				fprintf(stderr, "[!] Error: GetModuleBaseNameW: %ld\n", GetLastError());
-				return EXIT_FAILURE;
-			}
-			CloseHandle(hProcess);
-		}
-		else
-		{
-			fprintf(stderr, "[!] Error: OpenProcess() PID: %d -- Error Code: %ld\n",dwProcList[dwIndex], GetLastError());
-		}
+	    fprintf(stderr, "[!] Error: OpenProcess() PID: %d -- Error Code: %ld\n",dwProcList[dwIndex], GetLastError());
 	}
+    }
 
-	fprintf(stdout, "[+] Done!\n");
-	return ERROR_SUCCESS;
+    fprintf(stdout, "[+] Done!\n");
+    return ERROR_SUCCESS;
 }
